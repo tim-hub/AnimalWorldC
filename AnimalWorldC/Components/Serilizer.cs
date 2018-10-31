cd4512d3b61f74441961d7c4037ad486db971b04
tim-hub <tim_w@outlook.com>
2018-10-31 14:19:53 +1300

    serilizer worked

--- AnimalWorldC/Components/Serilizer.cs
+++ AnimalWorldC/Components/Serilizer.cs
@@ -21,10 +21,9 @@ namespace AnimalWorldC.Components
             {
                 sb.Append(
                     (JsonConvert.SerializeObject(
-                        element,
-                        Formatting.Indented
+                        element
                         )
-                    ) + "#"
+                    ) + '\n'
                 );
             }
             string temp = sb.ToString();
@@ -33,6 +32,43 @@ namespace AnimalWorldC.Components
 
             sw.Write(sb);
             sw.Close();
+        }
+
+        public static List<Element> DeSerilze(string path)
+        {
+            List<Element> elements = new List<Element>();
+
+            StreamReader sr = new StreamReader(path);
+
+            string theDots = sr.ReadToEnd();
+
+            sr.Close();
+
+            string[] theLines = theDots.Split('\n');
+
+            foreach (string line in theLines)
+            {
+                if (line != "")
+                {
+                    Element element;
+
+                    if (line.Contains("Scissors"))
+                    {
+                        element = JsonConvert.DeserializeObject<Scissors>(line);
+                    }
+                    else if (line.Contains("Rock")){
+                        element = JsonConvert.DeserializeObject<Rock>(line);
+
+                    }else {
+                        element = JsonConvert.DeserializeObject<Paper>(line);
+                    }
+
+                    
+                    elements.Add(element);
+                }
+            }
+
+            return elements;
         }
 
         
